330bee814a5bdc7d604444e48495a89276d60c4c
tokyojack2001@yahoo.com <tokyojack2001@yahoo.com>
2017-10-21 01:42:19 +0000

    Fix array of faces mistake & added a test.js file

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var faces = require('./faces').faces;
 
 exports.get = function(amount) {
-    var rn = Math.floor(Math.random() * faces.length);
     if (arguments.length === 0) {
-        return faces[rn];
+        return faces[getRandomNumber()];
     } else {
         var finalFaces = [];
-        
+
         for (var i = 0; i < amount; i++) {
-            finalFaces.push(faces[rn]);
+            finalFaces.push(faces[getRandomNumber()]);
         }
-        
+
         return finalFaces;
     }
 };
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * faces.length);
+}
---
+++ test.js
@@ -0,0 +1,4 @@
+var randomTextFaces = require('random-text-faces');
+  
+console.log(randomTextFaces.get()); //Returns a random face
+console.log(randomTextFaces.get(5)); //Returns an array of 5 different faces
