f693216bbbafe59e90345c9dfd2f8157aef52e07
Jack Clarke <tokyojack2001@yahoo.com>
2018-02-18 03:19:09 -0800

    Delete test.js

--- test.js
+++
@@ -1,4 +0,0 @@
-var randomTextFaces = require('random-text-faces');
-  
-console.log(randomTextFaces.get()); //Returns a random face
-console.log(randomTextFaces.get(5)); //Returns an array of 5 different faces
